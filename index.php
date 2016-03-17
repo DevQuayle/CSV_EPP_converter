408e93785d66532d8b3336b7f74f0ffcaa19e7ca
Slawek <krol.slawek1@gmail.com>
2016-03-17 23:58:54 +0100

    Chars code problem

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 	}
 </style>
 
+
 <?php
-header('Content-Type: text/html; charset=utf-8');
-	
+header('Content-Type: text/html; charset=CP852');
+//header("Content-Type: text/html; charset=windows-1250");	
 try{
 	$lines = file('plik.csv');
 	
@@ -43,11 +44,12 @@ try{
 // echo "</pre>";
 // exit();
 
+
 $begining = '[INFO]
-"1.05",3,1250,"Subiekt GT","Elkomp","Elkomp 2016","Elkomp Jacek K¹dzio³ka","Limanowa","34-600","Krótka  12","737-175-79-65","1","Sklep ul. Krótka 12",,,1,20160311114455,20160311114455,"Œliwa  Dawid",20160311114456,"Polska","PL","PL 7371757965",1
+"1.05",3,1250,"Subiekt GT","Elkomp","Elkomp 2016","Elkomp Jacek Kądziołka","Limanowa","34-600","Krótka  12","737-175-79-65","1","Sklep ul. Krótka 12",,,1,20160311114455,20160311114455,"Śliwa  Dawid",20160311114456,"Polska","PL","PL 7371757965",1
 
 [NAGLOWEK]
-"FZ",,,,,,,,,,,"9451981163","TOPTEL ANNA I WIKTOR WO£KOWICZ SPÓ£KA JA","TOPTEL ANNA I WIKTOR WO£KOWICZ SPÓ£KA JAWNA","Kraków","31-221","Bia³opr¹dnicka 15A","9451981163","Zakup","Zakup towarów lub us³ug","Kraków",20160309000000,20160309000000,20160309000000,30,1,"Cena ostatniej dost.",,,,,,,,,,,0,0,1,3,,,,0.0000,0.0000,"PLN",1.0000,,,,,0,0,0,,0.0000,,0.0000,"Polska","PL",0
+"FZ",,,,,,,,,,,"9451981163","TOPTEL ANNA I WIKTOR WOŁKOWICZ SPÓŁKA JA","TOPTEL ANNA I WIKTOR WO£KOWICZ SPÓŁKA JAWNA","Kraków","31-221","Białoprądnicka 15A","9451981163","Zakup","Zakup towarów lub usług","Kraków",20160309000000,20160309000000,20160309000000,30,1,"Cena ostatniej dost.",,,,,,,,,,,0,0,1,3,,,,0.0000,0.0000,"PLN",1.0000,,,,,0,0,0,,0.0000,,0.0000,"Polska","PL",0
 
 [ZAWARTOSC]';
 
@@ -58,13 +60,13 @@ $secondContent = '';
 
 try{
 	for ($i=1; $i < count($explode) ; $i++) { 
-		$code = str_replace("\n", "",$explode[$i][6]);
-		
+		//$code = str_replace("\n\r", "",$explode[$i][6]);
+		$code = str_replace(array("\r", "\n"), '', $explode[$i][6]);
 		$firstContent .= '
 '.$i.',1,"'.$code.'",1,0,0,1,0.0000,0.0000,"szt",'.$explode[$i][1].','.$explode[$i][1].','.$explode[$i][2].',,,23.0000,,,,,,';
 
 		$secondContent .= '
-1,"'.$code.'",,"'.$code.'","'.$explode[$i][0].'",,"'.$explode[$i][0].'","'.$explode[$i][0].'",,"szt.","23",0,"23",0,0,0.0000,"szt.",0,"PLN",,,0.0000,0,,,0,"szt.",0.0000,0.0000,,0,,0,0,,,,,,,,';
+1,"'.$code.'",,"'.$code.'","'.$explode[$i][0].'","'.$explode[$i][0].'","'.$explode[$i][0].'",,,"szt.","23",0,"23",0,0,0.0000,"szt.",0,"PLN",,,0.0000,0,,,0,"szt.",0.0000,0.0000,,0,,0,0,,,,,,,,';
 	}
 	echo "<div class='good'>OK: Dane zostały poprawnie przetworzone.  </div>";
 }catch (Exception $e) {
@@ -83,11 +85,13 @@ $litleContent ='
 [ZAWARTOSC]';
 
 
-$fullContent = $begining.$firstContent.$litleContent.$secondContent;
+$fullContent = $begining.$firstContent.$litleContent.$secondContent."\n";
+//$fullContent = iconv(mb_detect_encoding($fullContent), "ISO-8859-2//TRANSLIT", $fullContent);
 
+//$fullContent = htmlentities($fullContent, ENT_COMPAT, "CP852");
 
 try {
-	$fp = fopen("plik.txt", "w");
+	$fp = fopen("plik.epp", "w");
 	fputs($fp, $fullContent);
 	fclose($fp);
 	echo "<div class='good'>OK: Plik został poprawnie zapisany.  </div>";
